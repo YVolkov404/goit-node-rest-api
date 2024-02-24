2431e9a23318ccd0ebdd7d41b77cd9bc9e1d2a78
Yuriy Volkov <sir.volkov@outlook.com>
2024-02-24 14:34:08 +0200

    upd: path fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 
-const contactsRouter = require("./routes/api/contactsRouter");
+const contactsRouter = require("./routes/contactsRouter");
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contactsRouter", contactsRouter);
+app.use("./routes/contactsRouter.js", contactsRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
