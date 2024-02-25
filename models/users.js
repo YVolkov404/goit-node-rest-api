df908f8d0b5d73fca6e9cbf7289111ac9d721ecf
Yuriy Volkov <sir.volkov@outlook.com>
2024-02-25 13:38:14 +0200

    upd: step _1 done

---
+++ models/users.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+const mongooseError = require("../helpers/mongooseError");
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 8,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: emailRegexp,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", mongooseError);
+const User = model("user", userSchema);
+
+module.exports = User;
